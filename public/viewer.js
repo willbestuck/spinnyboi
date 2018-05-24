3c13c5be7559b5841e5e950a5d2ae3a2e44fbf32
willbestuck <wbstueck@gmail.com>
2018-05-24 15:38:54 -0700

    we're spinning baby

--- public/viewer.js
+++ public/viewer.js
@@ -74,24 +74,3 @@ $(function() {
         updateBlock(color);
     });
 });
-
-
-// Fidget Spinner
-var winWidth = $(window).width(),
-    winHeight = $(window).height(),
-    wrapper = $("#wrapper"),
-    spinner = $("#fidget-spinner");
-
-// resize it
-wrapper.width(winWidth/2);
-wrapper.height(winWidth/2);
-
-// spin it
-Draggable.create(spinner, {
-    type: "rotation", 
-    throwProps: true,
-    minDuration: 5,
-    maxDuration: 30,
-    throwResistance: 100,
-    overshootTolerance: 1
-});
